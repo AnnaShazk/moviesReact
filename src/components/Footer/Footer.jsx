4210f878e098bcee587a4e08202b0031030ab8fb
AnnaShazk <dream1nd.find@gmail.com>
2024-01-05 19:35:12 +0100

    add some fix to code

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      Footer
+    </>
+  );
+};
+
+export default Footer;
--- src/components/Main/Home/TopSection/Movies.jsx
+++ src/components/Main/Home/TopSection/Movies.jsx
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import { useState } from "react";
-const Movies = ({ movies }) => {
-  return (
-    <>
-      Hi
-    </>
-  );
-=======
 import React from "react";
 
 const Movies = () => {
   return <div>Movies</div>;
->>>>>>> 8f1f6cc69a28c5127873a0e7ad25b279a510b9e1
 };
 
 export default Movies;
