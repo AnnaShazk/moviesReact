75849089f80a19f7eefe70f6fe68ed6a8ffe4b3a
CiaranW12 <ciaran.walker12@gmail.com>
2024-01-09 15:18:48 +0100

    coded favourites, havent called it yet tho

--- src/components/Main/Home/MainSection/Home.jsx
+++ src/components/Main/Home/MainSection/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
   <div className="main_wrapper grid gri">
     <MovieDetails />
     <PopularMoviesSection />
+    
   </div>;
 };
 
--- src/components/Main/Home/MainSection/PopularMoviesSection.jsx
+++ src/components/Main/Home/MainSection/PopularMoviesSection.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faStar } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../../../assets/logo.png";
