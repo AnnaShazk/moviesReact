a7be5c5f97415ecd8ef9992adf44685c92c5a5df
AnnaShazk <dream1nd.find@gmail.com>
2024-01-09 16:02:58 +0100

    add some changes

--- src/components/Main/Home/MainSection/Home.jsx
+++ src/components/Main/Home/MainSection/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import MovieDetails from "./MovieDetails";
 import PopularMoviesSection from "./PopularMoviesSection";
-import poster from "../../../../assets/poster.png"
+import poster from "../../../../assets/poster.png";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faStar } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "flowbite-react";
 
 const Home = () => {
-
   const bagroundImage = poster;
 
   const [MovieDetails, setMovieDetails] = useState([]);
-/* 
+  /* 
   setMovieDetails({ title: "Avengers", year: "2021" }) */
 
   return (
@@ -17,18 +19,43 @@ const Home = () => {
       <div
         style={{
           backgroundImage: `url(${bagroundImage})`,
-          backgroundRepeat: "no-repeat",
+          backgroundRepeat: "repeat-y",
           backgroundSize: "cover",
           width: "100%",
           height: "50vw",
         }}
       >
-        <div className="flex flex-col items-start justify-start pt-5 h-full">
-          <h1 className="text-5xl font-bold text-white">Avengers</h1>
-          <h3 className="text-2xl font-semibold text-white">2021</h3>
+        <div className="flex flex-col max-w-sm ">
+          <h1 className="text-4xl bottom-0  font-bold text-white pl-5 pt-9">
+            Kung Fu Panda
+          </h1>
+          <div className="flex  items-center gap-5 p-5">
+            <FontAwesomeIcon icon={faStar} color="yellow" />
+            <h3 className="text-white font-semibold  mt-1">8.2</h3>
+            <Button className="rounded-3xl h-7 bg-emerald-600">
+              Completed
+            </Button>
+            <div className="flex gap-2">
+              <h2 className="text-white">Action</h2>
+              <h2 className="text-white">2023</h2>
+            </div>
+            <div className="flex flex-row"></div>
+          </div>
+          <p className="max-w-sm pl-5 pt-1 text-white">
+            Kung Fu Panda is an animated film about Po, a clumsy panda, who is
+            chosen as the Dragon Warrior to fight the formidable villain Tai
+            Lung.
+          </p>
+          <div className="flex gap-2 pl-5 pt-4">
+            <Button className="max-w-sm rounded-3xl h-7 bg-emerald-600">
+              Watch Now
+            </Button>
+            <Button className="max-w-sm rounded-2xl h-7 bg-emerald-600">
+              Add to Watchlist
+            </Button>
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
