871e81bd58fa8427f831b6b986fada079535c38a
AnnaShazk <dream1nd.find@gmail.com>
2024-01-13 15:32:33 +0100

    in process

--- src/components/CompanySection/CompanySection.jsx
+++ src/components/CompanySection/CompanySection.jsx
@@ -74,28 +74,31 @@ function CompanySection(darkMode) {
     },
   ];
 
-return (
-  <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16 dark:bg-black">
-    {(darkMode ? productionHouseListBlackBackground : productionHouseList).map(
-      (item) => (
-        <div
-          className="border-[2px] rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-xl"
-          key={item.id}
-        >
-          <video
-            src={item.video}
-            autoPlay
-            loop
-            playsInline
-            muted
-            className="absolute z-0 top-0 rounded-md opacity-0 hover:opacity-50"
-          />
-          <img src={item.image} className="w-full z-[1] opacity-100" />
-        </div>
-      )
-    )}
-  </div>
-);
+  return (
+    <>
+      <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16 dark:bg-black">
+        {(darkMode
+          ? productionHouseListBlackBackground
+          : productionHouseList
+        ).map((item) => (
+          <div
+            className="border-[2px] rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-xl"
+            key={item.id}
+          >
+            <video
+              src={item.video}
+              autoPlay
+              loop
+              playsInline
+              muted
+              className="absolute z-0 top-0 rounded-md opacity-0 hover:opacity-50"
+            />
+            <img src={item.image} className="w-full z-[1] opacity-100" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default CompanySection;
